545de0d2176925fcba3c4571907a388d86ac0624
cristianccgg <cristianccggg@gmail.com>
2024-06-17 19:20:51 -0500

    updated

---
+++ src/javascript.js
@@ -0,0 +1,8 @@
+const errors = document.querySelectorAll(".error");
+const submitBtn = document.getElementById("submitBtn");
+const formSubmit = document.getElementById("form");
+const inputs = document.querySelectorAll(".input")
+
+formSubmit.addEventListener("submit", () => {
+    if()
+});
